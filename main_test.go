0613365844d2ae24624a66b7b08fe197835bf439
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-06-01 20:16:13 -0500

    New test

--- main_test.go
+++ main_test.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"regexp"
 	"strings"
 	"testing"
 )
 
 var (
-	TestFileContent      string = `<svg fill="#4CAF50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path d="m40 10h-32c-2.21 0-3.98 1.79-3.98 4l-.02 20c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4v-20c0-2.21-1.79-4-4-4m-18 6h4v4h-4v-4m0 6h4v4h-4v-4m-6-6h4v4h-4v-4m0 6h4v4h-4v-4m-2 4h-4v-4h4v4m0-6h-4v-4h4v4m18 14h-16v-4h16v4m0-8h-4v-4h4v4m0-6h-4v-4h4v4m6 6h-4v-4h4v4m0-6h-4v-4h4v4"/></svg>`
+	TestFileContent string = `<svg fill="#4CAF50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path d="m40 10h-32c-2.21 0-3.98 1.79-3.98 4l-.02 20c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4v-20c0-2.21-1.79-4-4-4m-18 6h4v4h-4v-4m0 6h4v4h-4v-4m-6-6h4v4h-4v-4m0 6h4v4h-4v-4m-2 4h-4v-4h4v4m0-6h-4v-4h4v4m18 14h-16v-4h16v4m0-8h-4v-4h4v4m0-6h-4v-4h4v4m6 6h-4v-4h4v4m0-6h-4v-4h4v4"/></svg>`
+
 	TestFileContentBytes []byte = []byte(TestFileContent)
-	ToAddBytes           []byte
+
+	ToFindBytes    []byte
+	ToReplaceBytes []byte
+
+	ReToFindBytes *regexp.Regexp
 )
 
 func TestInit(t *testing.T) {
-	ToFind = "cyan"
-	ToReplace = "green"
+	ToFind = "green"
+	ToReplace = "cyan"
 	_setFindReplace()
+
+	ToFindBytes = []byte(ToFind)
+	ToReplaceBytes = []byte(ToReplace)
+
+	ReToFindBytes = regexp.MustCompile(ToFind)
 }
 
 func BenchmarkStringsReplaceAll(b *testing.B) {
@@ -32,6 +43,12 @@ func BenchmarkStringsReplaceOnce(b *testing.B) {
 func BenchmarkRegexpReplace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ReToFind.ReplaceAllString(TestFileContent, ToReplace)
+	}
+}
+
+func BenchmarkRegexpReplaceBytes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ReToFindBytes.ReplaceAll(TestFileContentBytes, ToReplaceBytes)
 	}
 }
 
