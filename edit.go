0bec1d32f6f5bdf838c2d96bfea8f17336d16461
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-06-01 22:17:21 -0500

    Split file in 2

---
+++ edit.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func editRecursive() error {
+	return filepath.Walk(SrcDir, _walkReplace)
+}
+
+func editOne() error {
+	return editFileFromPath(DstSvg, SrcSvg)
+}
+
+func _walkReplace(path string, fi os.FileInfo, err error) error {
+
+	if err != nil {
+		return err
+	}
+
+	if filepath.Ext(path) != ".svg" {
+		return nil
+	}
+
+	dstPath := fmtDst(path)
+	if err = _mkDstDir(dstPath); err != nil {
+		LogErr(err)
+		return nil
+	}
+
+	srcPath := path
+
+	if err = editFileFromPath(dstPath, srcPath); err != nil {
+		LogErr(err)
+		return nil
+	}
+
+	return nil
+}
+
+func _mkDstDir(path string) error {
+	dir := filepath.Dir(path)
+	return mkDir(dir)
+}
+
+func editFileFromPath(dstPath string, srcPath string) error {
+
+	if _isPathSymlink(srcPath) {
+		// return _copySymlinkFromPath(dstPath, srcPath)
+		return nil
+	}
+
+	fileBytes, err := ioutil.ReadFile(srcPath)
+	if failedToReadFile := (err != nil); failedToReadFile {
+		LogErr(err)
+		return _copyFromPath(dstPath, srcPath)
+	}
+
+	if isEmptyFile := (len(fileBytes) == 0); isEmptyFile {
+		return nil
+	}
+
+	var editedFileBytes []byte
+	if wasEdited := _editFileBytes(&fileBytes, &editedFileBytes); !wasEdited {
+		return nil
+	}
+
+	if somethingTerribleHappened := (len(editedFileBytes) == 0); somethingTerribleHappened {
+		return nil
+	}
+
+	newFile, err := os.Create(dstPath)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	if err = _bytesToFile(&editedFileBytes, newFile); err != nil {
+		return err
+	}
+
+	TotalEdited++
+	Log(dstPath)
+
+	return nil
+}
+
+func _editFileBytes(fileBytes *[]byte, editedFileBytes *[]byte) (wasEdited bool) {
+
+	wasEdited = _replace(editedFileBytes, fileBytes)
+	if wasEdited {
+		return
+	}
+
+	if shouldAddFill := (DoAddFill && !_hasFill(fileBytes)); !shouldAddFill {
+		return
+	}
+
+	wasEdited = _addFill(editedFileBytes, fileBytes)
+	return
+}
+
+func _replace(fileBytes *[]byte, editedFileBytes *[]byte) (wasEdited bool) {
+
+	*editedFileBytes = bytes.Replace(*fileBytes, ToFindBytes, ToReplaceBytes, -1)
+	// *editedFileBytes = ReToFind.ReplaceAll(*fileBytes, ToReplaceBytes)
+
+	wasEdited = (!bytes.Equal(*editedFileBytes, *fileBytes))
+	return
+}
+
+func _addFill(fileBytes *[]byte, editedFileBytes *[]byte) (wasEdited bool) {
+
+	*editedFileBytes = ReAddFill.ReplaceAll(*fileBytes, ToFillBytes)
+
+	wasEdited = (!bytes.Equal(*editedFileBytes, *fileBytes))
+	return
+}
+
+func _hasFill(fileBytes *[]byte) bool {
+	return bytes.Contains(*fileBytes, []byte("fill=")) ||
+		bytes.Contains(*fileBytes, []byte("fill:"))
+}
+
+func _bytesToFile(editedFileBytes *[]byte, newFile *os.File) error {
+
+	if _, err := newFile.Write(*editedFileBytes); err != nil {
+		return err
+	}
+
+	return newFile.Sync()
+}
--- file.go
+++ file.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 )
 
 func getPwd() string {
@@ -17,14 +14,6 @@ func getPwd() string {
 	}
 
 	return pwd
-}
-
-func editRecursive() error {
-	return filepath.Walk(SrcDir, _walkReplace)
-}
-
-func editOne() error {
-	return editFileFromPath(DstSvg, SrcSvg)
 }
 
 func mkDir(path string) error {
@@ -41,32 +30,6 @@ func _pathExists(path string) bool {
 	return true
 }
 
-func _walkReplace(path string, fi os.FileInfo, err error) error {
-
-	if err != nil {
-		return err
-	}
-
-	if filepath.Ext(path) != ".svg" {
-		return nil
-	}
-
-	dstPath := fmtDst(path)
-	if err = _mkDstDir(dstPath); err != nil {
-		LogErr(err)
-		return nil
-	}
-
-	srcPath := path
-
-	if err = editFileFromPath(dstPath, srcPath); err != nil {
-		LogErr(err)
-		return nil
-	}
-
-	return nil
-}
-
 func _isPathSymlink(path string) bool {
 
 	fi, err := os.Lstat(path)
@@ -76,53 +39,6 @@ func _isPathSymlink(path string) bool {
 	}
 
 	return (fi.Mode()&os.ModeSymlink == os.ModeSymlink)
-}
-
-func _mkDstDir(path string) error {
-	dir := filepath.Dir(path)
-	return mkDir(dir)
-}
-
-func editFileFromPath(dstPath string, srcPath string) error {
-
-	if _isPathSymlink(srcPath) {
-		// return _copySymlinkFromPath(dstPath, srcPath)
-		return nil
-	}
-
-	fileBytes, err := ioutil.ReadFile(srcPath)
-	if failedToReadFile := (err != nil); failedToReadFile {
-		LogErr(err)
-		return _copyFromPath(dstPath, srcPath)
-	}
-
-	if isEmptyFile := (len(fileBytes) == 0); isEmptyFile {
-		return nil
-	}
-
-	var editedFileBytes []byte
-	if wasEdited := _editFileBytes(&fileBytes, &editedFileBytes); !wasEdited {
-		return nil
-	}
-
-	if somethingTerribleHappened := (len(editedFileBytes) == 0); somethingTerribleHappened {
-		return nil
-	}
-
-	newFile, err := os.Create(dstPath)
-	if err != nil {
-		return err
-	}
-	defer newFile.Close()
-
-	if err = _bytesToFile(&editedFileBytes, newFile); err != nil {
-		return err
-	}
-
-	TotalEdited++
-	Log(dstPath)
-
-	return nil
 }
 
 func _copyFromPath(dstPath string, srcPath string) error {
@@ -149,52 +65,6 @@ func _copyFile(dst *os.File, src *os.File) error {
 	}
 
 	return dst.Sync()
-}
-
-func _editFileBytes(fileBytes *[]byte, editedFileBytes *[]byte) (wasEdited bool) {
-
-	wasEdited = _replace(editedFileBytes, fileBytes)
-	if wasEdited {
-		return
-	}
-
-	if shouldAddFill := (DoAddFill && !_hasFill(fileBytes)); !shouldAddFill {
-		return
-	}
-
-	wasEdited = _addFill(editedFileBytes, fileBytes)
-	return
-}
-
-func _replace(fileBytes *[]byte, editedFileBytes *[]byte) (wasEdited bool) {
-
-	*editedFileBytes = bytes.Replace(*fileBytes, ToFindBytes, ToReplaceBytes, -1)
-	// *editedFileBytes = ReToFind.ReplaceAll(*fileBytes, ToReplaceBytes)
-
-	wasEdited = (!bytes.Equal(*editedFileBytes, *fileBytes))
-	return
-}
-
-func _addFill(fileBytes *[]byte, editedFileBytes *[]byte) (wasEdited bool) {
-
-	*editedFileBytes = ReAddFill.ReplaceAll(*fileBytes, ToFillBytes)
-
-	wasEdited = (!bytes.Equal(*editedFileBytes, *fileBytes))
-	return
-}
-
-func _hasFill(fileBytes *[]byte) bool {
-	return bytes.Contains(*fileBytes, []byte("fill=")) ||
-		bytes.Contains(*fileBytes, []byte("fill:"))
-}
-
-func _bytesToFile(editedFileBytes *[]byte, newFile *os.File) error {
-
-	if _, err := newFile.Write(*editedFileBytes); err != nil {
-		return err
-	}
-
-	return newFile.Sync()
 }
 
 // func _copySymlinkFromPath(dstPath string, srcPath string) error {
